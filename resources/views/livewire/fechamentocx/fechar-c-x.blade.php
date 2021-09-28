11555900ccdfcfd06f2470341a2955e22e2484bf
Reginaldo Miranda <reginaldobrain@gmail.com>
2021-09-28 16:48:05 -0300

    firma 2809 16

--- resources/views/livewire/fechamentocx/fechar-c-x.blade.php
+++ resources/views/livewire/fechamentocx/fechar-c-x.blade.php
@@ -1,4 +1,4 @@
 <div>
     <p>estou na fechar cx blade</p>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    @include('livewire.fechamentocx.tableFecharcx')
 </div>
