4715016251975bb5540baa3a9e46e16e3cc84500
Reginaldo Miranda <reginaldobrain@gmail.com>
2021-09-28 16:11:19 -0300

    firma 2809 16

---
+++ app/Http/Livewire/FecharCX.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class FecharCX extends Component
+{
+    public function render()
+    {
+        return view('livewire.fechar-c-x');
+    }
+}
---
+++ resources/views/livewire/fechamentocx/fechar-c-x.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <p>estou na fechar cx blade</p>
+    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+</div>
